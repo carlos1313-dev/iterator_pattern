f2a8c96b8176ed5931844abfd164dcdc0d04fad6
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-05-13 14:52:07 -0500

    Add: First classes

---
+++ src/main/java/com/mycompany/iterator_pattern/Iterator_pattern.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iterator_pattern;
+
+/**
+ *
+ * @author sangr
+ */
+public class Iterator_pattern {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
